a44b45a0697a9bb93e19e60c74ccc89d6d8f2db1
mgasner <max@naviasystems.com>
2011-06-20 18:16:43 -0700

    insertion sort in python

---
+++ chapter_2/insertion_sort.py
@@ -0,0 +1,9 @@
+def insertion_sort(arr):
+  for i in range(len(arr)):
+    j = i
+    while (j > 0) and (arr[j] < arr[j - 1]):
+      x = arr[j]
+      arr[j] = arr[j - 1]
+      arr[j - 1] = x
+      j = j - 1
+  return arr
