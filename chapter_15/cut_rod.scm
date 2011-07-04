60f867a07b177516f358d5271cc432289994cd7c
mgasner <max@naviasystems.com>
2011-07-04 13:34:37 -0700

    naive rod cutting

---
+++ chapter_15/cut_rod.scm
@@ -0,0 +1,10 @@
+(define (cut-rod p n)
+  (if (= n 0)
+      0
+      (let ((q (vector-ref p 0)))
+        (let loop ((i 0))
+          (if (= i n)
+              q
+              (begin
+                (set! q (max q (+ (vector-ref p i) (cut-rod p (- n i)))))
+                (loop (+ i 1))))))))
